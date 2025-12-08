55761ac6323c6976c4505dea2041c9a26a5d4a22
Farhan Jarif Nibir <nibir119@gmail.com>
2025-12-09 00:41:20 +0600

    Add Laravel Sanctum for API authentication and create migration for personal access tokens

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->group('api',[
+            EnsureFrontendRequestsAreStateful::class, 'throttle:api',SubstituteBindings::class 
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
